d8a77d4e1997888834592189848ffa99fcb8a91c
thinkGrow <rubaitreshad@gmail.com>
2025-03-23 14:55:46 +0600

    Second Commit

---
+++ strings pyt/intro.py
@@ -0,0 +1,50 @@
+
+# number = int(input("Enter a number: "))
+
+# number = input("Enter a number: ")
+
+# number = float(number)
+
+# print(number + 2)
+
+# finding length
+
+# string = 'Computer Science'
+# length = len(string)
+# print(length)
+
+# password validation
+
+# password = input("Enter a password: ")
+# length = len(password)
+# if length < 6:
+#     print("The password you have entered is not long enough")
+# else:
+#     print("Length of password OK")
+
+# print traversal
+
+# animalName = 'monkey'
+
+# for i in range (0, len(animalName)):
+#     print(animalName[i])
+
+# string slicing
+
+# string = 'Computer Science' 
+
+# for i in range(0, len(string)):
+#     print(string[11])
+
+# for i in range (0, len(string)):
+#     print(f'{i}th index = {string[i]}')
+
+# changing characters to lower case
+# string = 'Computer Science'
+# string = string.lower()
+# print(string)
+
+# uppercase
+# string = 'Computer Science'
+# string = string.upper()
+# print(string)
---
+++ strings pyt/second.py
@@ -0,0 +1,40 @@
+# string slicing
+
+# string = 'Computer Science'
+# string = string[3:6]
+# print(string)
+
+# string check
+
+# string = 'Computer Science'
+# check_string = 'Science'
+# if check_string in string:
+#     print('Found')
+
+# book way
+# string = 'Computer Science'
+# present = 'put' in string
+# print(present)
+# temp_string = string.upper()
+# present = 'PUT' in temp_string
+# print(present)
+
+# concatenation
+
+# first_name = 'Arham'
+# last_name = 'Syed'
+
+# full_name = 'Arham' + ' ' + last_name
+
+# print(full_name)
+
+# concatenating numbers
+
+# int and string cannot be concatenated
+# length = 13
+# print('The length is' + length)
+
+# int needs to be converted to string first
+# length = 13
+# length = str(length)
+# print('The length is: ' + length)
