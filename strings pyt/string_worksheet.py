c98250632c1527ec44976bee072a04d60f8c1bd8
thinkGrow <rubaitreshad@gmail.com>
2025-03-23 16:16:36 +0600

    Question 8

---
+++ strings pyt/string_worksheet.py
@@ -0,0 +1,17 @@
+# 8. Enhance the program to also verify if the password contains at least one uppercase letter.
+
+# password = input("Enter your password: ")
+
+# for character in password:
+#     if character.isupper():
+#         uppercase_found = True
+#     else:
+#         uppercase_found = False
+
+# if uppercase_found:
+#     print("Password has atleast one uppercase letter.")
+# else:
+#     print("Password must contain atleast one uppercase letter.")
+
+
+
