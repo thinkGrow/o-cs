d206d6e0cd3b550ac9e53660c79ceef62c08f0e4
thinkGrow <rubaitreshad@gmail.com>
2025-03-23 16:43:58 +0600

    String Worksheet - Q12

--- strings pyt/string_worksheet.py
+++ strings pyt/string_worksheet.py
@@ -14,4 +14,20 @@
 #     print("Password must contain atleast one uppercase letter.")
 
 
+# 12. Modify the loop so that it prints the letters in reverse order.
+
+# string = input("Enter your string: ")
+
+# reversed_string = ""
+
+# for i in range(len(string) - 1, -1, -1):
+#     reversed_string = reversed_string + string[i]
+
+# print("the right answer is:", reversed_string)
+
+
+# Alternatively,
+# txt = "Hello World"
+# txt = txt [::-1]
+# print(txt)
 
